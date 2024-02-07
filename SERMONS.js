2d7cbedd358d6c42511fc7352a4fcae26477a68f
Gods-98 <159225741+Gods-98@users.noreply.github.com>
2024-02-07 10:29:02 +0300

    Add files via upload

---
+++ SERMONS.js
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<head><title>
+SERMONS
+
+
+</title></head>
+<body>
+<header>
+	<a href="index.html">
+	<img src="img/logo.jpg" alt="Tonny Hack" title="God's Hand Glorious Ministry.int">
+</a>
+<hr>
+<table cellspacing="5" cellpadding="6">
+<nav>
+<tr>
+<td><a href="HOME.html"> HOME</a></td>
+<td><a href="ABOUT US.html"> ABOUT US</a></td>
+<td><a href="index.html"> SERMONS</a></td>
+<td><a href="PROGRAMS.html"> PROGRAMS</a></td>
+<td><a href="RESOURCES.html"> RESOURCES</a></td>
+
+</tr>
+
+
+</nav>
+</table>
+
+</header>
+
+	<table>
+       <tr>
+       	<td width="40%" height="20">
+
+<ol><video src="videos/1.mp4" width="40%" controls></video>
+               </td>
+
+               <td>
+          Description
+               </td>
+           </tr>
+           <tr>
+                 <td width="40%" height="20">>
+    <video src="videos/2.mkv" width="40%" controls></video>
+                  </td>
+                  <td>
+description
+                  </td>
+           </tr>
+                <tr>
+                      
+                     <td width="40%" height="20">
+<video src="videos/3.mp4" width="40%" controls></video>
+                      </td>
+
+                      <td>
+              Description
+                      </td>
+                </tr>
+                <tr>
+                	<td width="40%" height="20">
+               <video src="videos/4.mp4" width="40%" controls></video>
+                	</td>
+                     <td>
+                Description
+                     </td>
+
+
+</tr>
+
+.body {
+    
+     main height:100vh;
+     background:url(../image/koko.jpg) no repeat
+      background-size:cover;
+       background-portion: center;
+         background-color:"red"
+    }   
+
+
+
+
+
+
+
